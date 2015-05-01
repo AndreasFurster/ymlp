cda710fdbc3e79977afebebaa8699102ae64eb74
AndreasFurster <andreas.furster@hotmail.com>
2015-05-01 22:51:18 +0200

    no message

--- js/controllers.js
+++ js/controllers.js
@@ -3,15 +3,15 @@ var controllers = angular.module('controllers', []);
 controllers.controller('login', ['$scope', '$http',
 	function ($scope, $http) {
 
-	});
+	}]);
 
 controllers.controller('start', ['$scope', '$http',
 	function ($scope, $http) {
 
-	});
+	}]);
 
 controllers.controller('email', ['$scope', '$http',
 	function ($scope, $http) {
 
-	});
+	}]);
 
